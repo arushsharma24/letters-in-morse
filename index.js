33f1e23f050b1e1e58f0e7e44ce956be0143a5dd
arushsharma <arush24astar@gmail.com>
2021-02-23 22:29:54 +0530

    balloon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function translate(){
     let message = document.getElementById('letter').innerHTML;
-    alert("Translated!");
+    alert("Not yet translated, but here's a balloon for you :🎈 ");
 }
 
 document.getElementById('submit').addEventListener('click', translate);
